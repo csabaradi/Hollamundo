82d53031f4cba3c3561244d9334c9949433a3f2b
Levente Guti <levente_guti@yahoo.com>
2017-11-27 11:00:30 +0200

    first commit

---
+++ src/main/java/ro/nttdata/Holamundo/App.java
@@ -0,0 +1,17 @@
+package ro.nttdata.Holamundo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+    {
+        System.out.println(getHelloWorld());
+    }
+    public static String getHelloWorld() {
+
+		return "Hola Mundo";
+    }
+}
